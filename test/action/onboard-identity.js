b319b0b041cec7e1b7d9aebc34eba9bccf98c2a7
David Harper <real.david.harper@gmail.com>
2023-06-09 20:20:58 -0700

    reddit onboard flow

--- test/action/onboard-identity.js
+++ test/action/onboard-identity.js
@@ -12,7 +12,7 @@ const onboardIdentity = async function ( $ ) {
       console.log(result);
 
       $.conforms( "action_onboard_identity_start", "post", result );
-      h.assert(result.redirect_url.startsWith("https://api.twitter.com/oauth/authorize?oauth_token="));
+      h.assert(result.redirect_url.startsWith("https://api.twitter.com/oauth/authorize?"));
     })),
 
     // await h.test( "twitter callback", h.target( "action-onboard-identity", async () => {
@@ -20,6 +20,29 @@ const onboardIdentity = async function ( $ ) {
     //     base_url: "https://twitter.com",
     //     oauth_token: "",
     //     oauth_verifier: ""
+    //   }});
+      
+    //   console.log(result);
+
+    //   $.conforms( "action_onboard_identity_callback", "post", result );
+    // })),
+
+    await h.test( "reddit redirect", h.target( "action-onboard-identity", async () => {
+      const result = await $.gobo.actionOnboardIdentityStart.post({ content: {
+        base_url: "https://www.reddit.com"
+      }});
+      
+      console.log(result);
+
+      $.conforms( "action_onboard_identity_start", "post", result );
+      h.assert(result.redirect_url.startsWith("https://www.reddit.com/api/v1/authorize?"));
+    })),
+
+    // await h.test( "reddit callback", h.target( "action-onboard-identity", async () => {
+    //   const result = await $.gobo.actionOnboardIdentityCallback.post({ content: {
+    //     base_url: "https://www.reddit.com",
+    //     state: "",
+    //     code: ""
     //   }});
       
     //   console.log(result);
