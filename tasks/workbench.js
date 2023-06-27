5baa3b093ff7414a9b5d9b0f81b995bcde5659a1
David Harper <real.david.harper@gmail.com>
2023-06-26 19:45:16 -0700

    incorporated workbench tests for Reddit and Mastodon to get polling handled.

--- tasks/workbench.js
+++ tasks/workbench.js
@@ -1,13 +1,13 @@
 import { getGOBO } from "./helpers.js";
 
-const run = async function (config) {
-  const gobo = await getGOBO(config);
-  const result = await gobo.actionWorkbench.post({ content: {
-    profile_url: "https://twitter.com/davidgobo1"
-  }});
+// const run = async function (config) {
+//   const gobo = await getGOBO(config);
+//   const result = await gobo.actionWorkbench.post({ content: {
+//     profile_url: "https://twitter.com/davidgobo1"
+//   }});
 
-  console.log(result);
-};
+//   console.log(result);
+// };
 
 
 // const run = async function (config) {
@@ -18,6 +18,37 @@ const run = async function (config) {
   
 //   console.log(result);
 // };
+
+
+// const run = async function (config) {
+//   const gobo = await getGOBO(config);
+//   const result = await gobo.actionWorkbench.post({ content: {
+//     profile_url: "https://www.reddit.com/user/davidgobo"
+//   }});
+
+//   console.log(result);
+// };
+
+
+// const run = async function (config) {
+//   const gobo = await getGOBO(config);
+//   const result = await gobo.actionPullIdentitySources.post({ content: {
+//     profile_url: "https://www.reddit.com/user/davidgobo"
+//   }});
+  
+//   console.log(result);
+// };
+
+
+const run = async function (config) {
+  const gobo = await getGOBO(config);
+  const result = await gobo.actionPullIdentitySources.post({ content: {
+    profile_url: "https://mastodon.social/@davidgobo"
+  }});
+  
+  console.log(result);
+};
+
 
 
 export {
