f56e2a4025a9f3d05b543de4ac644e2ffec8b1ac
David Harper <real.david.harper@gmail.com>
2023-06-30 00:15:05 -0700

    correction to issuer

--- test/helpers.js
+++ test/helpers.js
@@ -26,7 +26,7 @@ const getConfiguration = async function ( environment ) {
 
 const getToken = async function () {
   const response = await fetch(
-    "https://dev-j72vlrggk1ft8e8u.us.auth0.com/oauth/token", {
+    "https://auth.gobo.social/oauth/token", {
     method: "POST",
     headers: { 
       "content-type": "application/json"
