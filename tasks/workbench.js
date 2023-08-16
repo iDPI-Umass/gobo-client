45301dd8be99f9a2cad1a6d4c75e2ceb95621616
David Harper <real.david.harper@gmail.com>
2023-08-16 04:25:58 -0700

    text baseline for crossposting integration

--- tasks/workbench.js
+++ tasks/workbench.js
@@ -247,6 +247,26 @@ const tasks = {
     }});
   },
 
+  redditOnboardIdentity: async function (config) {
+    const gobo = await getGOBO(config);
+    const result = await gobo.actionOnboardIdentityStart.post({ content: {
+      base_url: "https://www.reddit.com"
+    }});
+
+    console.log(result);
+  },
+
+  redditOnboardIdentityFinish: async function (config) {
+    const gobo = await getGOBO(config);
+    const result = await gobo.actionOnboardIdentityCallback.post({ content: {
+      base_url: "https://www.reddit.com",
+      state: "",
+      code: ""
+    }});
+      
+    console.log(result);
+  },
+
   escapeTitles: async function (config) {
     const gobo = await getGOBO(config);
   
